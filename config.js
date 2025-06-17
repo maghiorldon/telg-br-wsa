2989e8817198c0ecb80b532f23f7aa9b453e4c49
maghiorldon <maggioeldon@my.jccmi.edu>
2025-06-18 00:42:38 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+module.exports = {
+  telegram: {
+    token: '7852353069:AAFEMafncSBX6vsdiO-gzYvx0W3cbC7BQyc',
+    chatId: '-4968958897',
+    port: 3000 // Telegram機器人用API server端口
+  },
+  whatsapp: {
+    chatId: 'WhatsApp群組ID', // 例如 '123456789-123456@g.us'
+    port: 4000 // WhatsApp機器人API server端口
+  }
+};
